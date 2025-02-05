4d1dc3a44196612896766e11f9b78791af6ffcf6
Varun Satya Sai Krishna Paladugula <117852619+pvsskrishna@users.noreply.github.com>
2025-02-05 20:12:41 +0530

    Add files via upload

---
+++ Day_1_Project_Band_Name_Generator/Brand_Name_Generator.py
@@ -0,0 +1,4 @@
+print('Welcome to the Brand Name Generator.')
+city_name = input(f"What is the name of the city you gre up in? \n")
+pet_name = input(f"What's your pet name? \n")
+print(f"Your brand name could be {city_name} {pet_name}")
