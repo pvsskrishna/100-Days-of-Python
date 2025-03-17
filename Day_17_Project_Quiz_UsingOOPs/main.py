7393ceeedaf313a739a601375965f1240b584bf7
Varun Satya Sai Krishna Paladugula <117852619+pvsskrishna@users.noreply.github.com>
2025-03-17 11:11:01 +0530

    Add files via upload

---
+++ Day_17_Project_Quiz_UsingOOPs/main.py
@@ -0,0 +1,29 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+for question in question_data:
+
+    #question_text = question['text']
+    question_text = question['question']
+
+    #question_answer = question['answer']
+    question_answer = question['correct_answer']
+
+    new_question = Question(q_text= question_text, q_answer= question_answer)
+    question_bank.append(new_question)
+
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_question():
+    quiz.new_question()
+
+print(f"You have completed the quiz!!!")
+print(f"Your final score is {quiz.score}/{quiz.question_number}")
+
+
+
+
+
